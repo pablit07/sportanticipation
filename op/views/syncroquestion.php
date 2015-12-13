90ec0c3ebad1f33637fd9c9818afb0eab9aa6458
paul k <paul.kohlhoff@gmail.com>
2015-12-13 09:25:17 -0700

    add test mode column

--- op/views/syncroquestion.php
+++ op/views/syncroquestion.php
@@ -26,9 +26,10 @@ $correct =   clean($_POST["correct"]);
 $test_question_id =  clean( $_POST["test_question_id"]);
 $test_question_answered =  clean( $_POST["test_question_answered"]);
 $test_question_answer_correct =   clean($_POST["test_question_answer_correct"]);
+$is_test_mode = clean($_POST["is_test_mode"]);
 
 
-$sql = "INSERT INTO st_question_summaries (`ts_test_id`,`ts_total_answered`,`ts_total_correct`,`ts_user_id`,`ts_started`,`ts_team`,`ts_title`,`ts_session_id`,`test_question_id`,`st_question_answered`,`test_question_answer_correct` )VALUES ('$test_id', '$test_question_count', '$correct', '$pid', '$sdate', '$test_team', '$test_title', '$testsessionid','$test_question_id','$test_question_answered ', '$test_question_answer_correct')";
+$sql = "INSERT INTO st_question_summaries (`ts_test_id`,`ts_total_answered`,`ts_total_correct`,`ts_user_id`,`ts_started`,`ts_team`,`ts_title`,`ts_session_id`,`test_question_id`,`st_question_answered`,`test_question_answer_correct`,`is_test_mode`)VALUES ('$test_id', '$test_question_count', '$correct', '$pid', '$sdate', '$test_team', '$test_title', '$testsessionid','$test_question_id','$test_question_answered ', '$test_question_answer_correct', '$is_test_mode')";
 
 if(mysqli_query($mysqli, $sql)){}
 
