f89f5769520ab3eebca160e43138706b2902cb68
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2016-02-10 10:24:58 -0700

    remove container wrapper

--- op/js/script2.js
+++ op/js/script2.js
@@ -350,7 +350,7 @@ function LoadRndVideo() {
     console.log(video);
     console.log(videofull);
 
-    document.getElementById("testcontainer").innerHTML = '<div id="netstatus" style="pull-left;"></div><div class="pull-right" style="position:relative; margin-right:100px;"><span id="plid"></span><small><span id="status"></span></small><span id="sessioninfo"></span></div><div id="loginbox" style="max-width:100%; text-align:center;"></div><div id="plid"></div><div id="sessioninfo"></div><div id="message"></div><div class="inforow" id="inforow"></div><div class="container"><div id="status"></div><div id="questionrow"></div><div id="feedbackrow"></div><div id="answerrow" class="answerrow"><div class="btn-group"><div id="buttonrow" class="buttonrow"></div></div><div class="videorow" id="videorow"></div></div>';
+    document.getElementById("testcontainer").innerHTML = '<div id="netstatus" style="pull-left;"></div><div class="pull-right" style="position:relative; margin-right:100px;"><span id="plid"></span><small><span id="status"></span></small><span id="sessioninfo"></span></div><div id="loginbox" style="max-width:100%; text-align:center;"></div><div id="plid"></div><div id="sessioninfo"></div><div id="message"></div><div class="inforow" id="inforow"></div><div id="status"></div><div id="questionrow"></div><div id="feedbackrow"></div><div id="answerrow" class="answerrow"><div class="btn-group"><div id="buttonrow" class="buttonrow"></div><div class="videorow" id="videorow"></div></div>';
 
     document.getElementById("videorow").innerHTML = '<div id="a"><video style="max-width:98%;" id="video"  autoplay><source src="../videos/001/' + video + '" type="video/mp4"/></video></div>';
 
@@ -604,7 +604,7 @@ function play_re() {
 
     document.getElementById("testcontainer").innerHTML += '<div class="videorow" id="videorow"></div>';
 
-    document.getElementById("videorow").innerHTML = '<div id="a"><video style="max-width:98%;" id="video"  autoplay><source src="../videos/001/' + video + '" type="video/mp4"/></video></div>';
+    document.getElementById("videorow").innerHTML = '<div id="a"><video height="90%;" id="video"  autoplay><source src="../videos/001/' + video + '" type="video/mp4"/></video></div>';
 
 }
 
